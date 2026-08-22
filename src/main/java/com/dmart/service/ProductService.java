4815543952a9ef83471345c7bf984d1f82b4285d
dhanashree-mahajan <dhanashrimahajan278@gmail.com>
2026-08-22 13:52:52 +0530

    Add product management APIs

---
+++ src/main/java/com/dmart/service/ProductService.java
@@ -0,0 +1,76 @@
+package com.dmart.service;
+
+import com.dmart.entity.Category;
+import com.dmart.entity.Product;
+import com.dmart.repository.CategoryRepository;
+import com.dmart.repository.ProductRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(
+            ProductRepository productRepository,
+            CategoryRepository categoryRepository) {
+
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Product addProduct(Product product) {
+
+        Long categoryId = product.getCategory().getId();
+
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() ->
+                        new RuntimeException("Category not found"));
+
+        product.setCategory(category);
+
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+
+        return productRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Product not found"));
+    }
+
+    public Product updateProduct(Long id, Product product) {
+
+        Product existingProduct = getProductById(id);
+
+        existingProduct.setName(product.getName());
+        existingProduct.setDescription(product.getDescription());
+        existingProduct.setPrice(product.getPrice());
+        existingProduct.setStockQuantity(product.getStockQuantity());
+
+        Long categoryId = product.getCategory().getId();
+
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() ->
+                        new RuntimeException("Category not found"));
+
+        existingProduct.setCategory(category);
+
+        return productRepository.save(existingProduct);
+    }
+
+    public void deleteProduct(Long id) {
+
+        Product product = getProductById(id);
+
+        productRepository.delete(product);
+    }
+}
